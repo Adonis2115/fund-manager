620422e96ad2dd7ce64f099d5deb00ef2e766180
Himanshu Pandey <himanshu21pandey@gmail.com>
2025-08-18 11:22:27 +0530

    migrations and submodule update

---
+++ sql/migrations/20250815102214_stocks_daily.sql
@@ -0,0 +1,39 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE
+    stocks (
+        id uuid PRIMARY KEY,
+        created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+        updated_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+        name VARCHAR(100) NOT NULL,
+        symbol VARCHAR(50) NOT NULL,
+        scriptType VARCHAR(50) NOT NULL,
+        industry VARCHAR(50),
+        isin VARCHAR(50),
+        fno boolean NOT NULL
+    );
+
+CREATE TABLE
+    daily (
+        id uuid PRIMARY KEY,
+        created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+        updated_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+        stockId uuid NOT NULL,
+        open DECIMAL,
+        high DECIMAL,
+        low DECIMAL,
+        close DECIMAL,
+        volume INTEGER,
+        timestamp DATE
+    );
+
+-- Establish the one-to-many relationship between stocks and daily
+ALTER TABLE daily
+ADD CONSTRAINT fk_daily_stockid
+FOREIGN KEY (stockId) REFERENCES stocks(id);
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+SELECT 'down SQL query';
+-- +goose StatementEnd
